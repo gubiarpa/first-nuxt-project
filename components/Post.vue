6175ec7ee5081257c4f011cf56de4cd61f6dfaae
gubiarpa <billy.arredondo@pucp.pe>
2021-08-08 22:42:09 -0500

    Add Post Component

---
+++ components/Post.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="post">
+    <p>{{ id }}. {{ name }}</p>
+    <p>{{ content }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Post',
+  props: ['id', 'name', 'content']
+}
+</script>
+
+<style>
+  .post {
+    padding: 1rem;
+    border: 1px solid whitesmoke;
+    margin: 1rem 0;
+  }
+</style>
